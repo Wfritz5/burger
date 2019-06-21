ec5c76b5ef31f82909b6bf915e47f05b85f10665
Will <wfritz5@gmail.com>
2019-06-20 23:33:22 -0600

    schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,7 @@
-
 INSERT INTO Burgers (id, name, devoured, date)
 VALUES
-	(1, 'Hamburger', 1, '2016-10-02 10:51:12'),
-	(2, 'CheeseBurger', 1, '2016-10-02 10:52:15'),
-	(3, 'Bacon Burger', 0, '2016-10-02 10:53:12'),
-	(4, 'Veggie DeluxeBurger', 0, '2016-10-02 10:54:22'),
-	(5, 'Fish Sandwich', 0, '2016-10-02 10:55:24');
+	(1, 'Hamburger', 1, '2019-06-20 10:51:12'),
+	(2, 'CheeseBurger', 1, '2019-06-20 10:52:15'),
+	(3, 'Bacon Burger', 0, '2019-06-20 10:53:12'),
+	(4, 'Veggie DeluxeBurger', 0, '2019-06-20 10:54:22'),
+	(5, 'Fish Sandwich', 0, '2019-06-20 10:55:24');
